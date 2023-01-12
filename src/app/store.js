e31af12798124c02d7ed5aa1325da1be45102dfb
Metehan Gulac <metehanglc@protonmail.com>
2023-01-12 22:29:37 +0100

    fix: rtk-query false configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../counter/counterSlice'
-import { jokesApi } from '../query/apiSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import counterReducer from "../counter/counterSlice";
+import { jokesApi } from "../query/apiSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    [jokesApi.reducerPath]:jokesApi.reducer,
-  }, 
-})
+    [jokesApi.reducerPath]: jokesApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(jokesApi.middleware),
+});
+
+setupListeners(store.dispatch);
